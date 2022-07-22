3f9d569d6a6bca47b0804f57566f4a6d0cf4b362
Alfonso0902 <Alfonso.da02@hotmail.com>
2022-07-22 12:10:50 +0200

    alfonso
    alfonso

---
+++ 1Enduro.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class cataloghi_EnduroBike : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnEnduroPark_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("1Enduro.aspx");
+    }
+    protected void btnMountainPark_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("3MoutainBike.aspx");
+
+    }
+
+    protected void btnRacePark_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("2RaceBike.aspx");
+
+    }
+}
---
+++ 2RaceBike.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class RaceBike : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected void btnEnduroPark_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("1Enduro.aspx");
+    }
+    protected void btnMountainPark_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("3MoutainBike.aspx");
+
+    }
+
+    protected void btnRacePark_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("2RaceBike.aspx");
+
+    }
+
+
+
+}
